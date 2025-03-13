61403625a4919926a08bc317a69dfc8861700b9a
Gabriel Alvarez <alvarezgbr01@gmail.com>
2025-03-13 08:08:51 -0300

    Aula sobre variáveis.

---
+++ Aula02/variaveis.js
@@ -0,0 +1,15 @@
+var numero = 2;
+let texto = "Hello";
+const oQueEisso = "?";
+
+console.log('Tipo var:', numero);
+
+numero = 13;
+
+console.log(texto + " O numero é " + numero);
+numero = 4
+console.log(`${texto} o número agora é ${numero}`)
+
+texto = 69; 
+
+console.log(texto + numero);
